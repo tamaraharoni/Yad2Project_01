b39835677ef32d50a959bd3bbbfd2c82905f132e
tamaraharoni <45860481+tamaraharoni@users.noreply.github.com>
2019-12-09 10:06:43 +0200

    Add files via upload

---
+++ Services/IDataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yad2Project.Services
+{
+        public interface IDataService<T>
+        {
+            List<T> DataList { get; set; }
+            List<T> InitializeData();
+            T GetItemById(int? id);
+            void AddItem(T item);
+        }
+
+}
